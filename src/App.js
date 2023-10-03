69e29588bd165a6e014a01342ef151090b2acb64
iamreinaldo <iamreinaldoneto@icloud.com>
2023-10-02 22:50:39 -0300

    completando o organo

--- src/App.js
+++ src/App.js
@@ -1,67 +1,126 @@
-import { useState } from 'react';
-import Banner from './components/Banner';
-import Formulario from './components/Formulario';
-import Time from './components/Time';
-import Rodape from './components/Rodape/rodape';
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Rodape from "./componentes/Rodape";
+import Time from "./componentes/Time";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
-      nome:'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      id: uuidv4(),
+      nome: 'Direção',
+      cor: '#57C278'
     },
     {
-      nome:'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      id: uuidv4(),
+      nome: 'Comercial',
+      cor: '#82CFFA'
     },
     {
-      nome:'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      id: uuidv4(),
+      nome: 'Suporte Interno',
+      cor: '#A6D157'
     },
     {
-      nome:'DevOps',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      id: uuidv4(),
+      nome: 'NOC',
+      cor: '#FF8A29'
     },
     {
-      nome:'UX e Design',
-      corPrimaria: '#D86EBF',
-      corSecundaria: '#FAE5F5'
+      id: uuidv4(),
+      nome: 'Suporte Externo',
+      cor: '#E06B69'
     },
     {
-      nome:'Mobile',
-      corPrimaria: '#FEBA05',
-      corSecundaria: '#FFF5D9'
+      id: uuidv4(),
+      nome: 'Infraestrutura',
+      cor: '#FF8A29'
     },
     {
-      nome:'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      id: uuidv4(),
+      nome: 'Administrativo',
+      cor: '#DB6EBF'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Financeiro',
+      cor: '#FFBA05'
+    },
+    {
+      id: uuidv4(),
+      nome: 'RH',
+      cor: '#FF8A29'
     }
+  ])
+
+  const inicial = [
+    {
+      id: uuidv4(),
+      nome: 'Reinaldo Neto',
+      cargo: 'CTO',
+      imagem: 'https://github.com/iamreinaldo.png',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Paulo Henrique',
+      cargo: 'CEO e Fundador',
+      imagem: 'https://github.com/iamreinaldo.png',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Patrícia Oquendo',
+      cargo: 'CFO e Fundadora',
+      imagem: 'https://github.com/iamreinaldo.png',
+      time: times[0].nome
+    },
+
   ]
 
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState(inicial)
 
-  const aoNovoColaboradorCadastrado = (colaborador) => {
-    setColaboradores([...colaboradores, colaborador])
+  function deletarColaborador(id){
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
   }
 
+  function mudarCorDoTime(cor, id){
+    setTimes(times.map(time =>{
+      if(time.id === id){
+        time.cor = cor
+      }
+      return time;
+    }))
+  }
+
+  function cadastarTime(novoTime){
+    setTimes([...times, {...novoTime, id: uuidv4()}])
+  }
+  
   return (
-    <div className="App">
+    <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorCadastrado(colaborador)}  />
-      {times.map(times => <Time
-         key={times.nome} 
-         nome={times.nome} 
-         corPrimaria={times.corPrimaria} 
-         corSecundaria={times.corSecundaria}
-         colaborador={colaboradores.filter(colaborador => colaborador.time === times.nome)}
-         />)}
-    <Rodape />
+      <Formulario 
+        cadastrarTime={cadastarTime}
+        times={times.map(time => time.nome)} 
+        aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} 
+      />
+      <section className="times">
+        <h1>Minha organização</h1>
+        {times.map((time, indice) => 
+          <Time 
+            mudarCor={mudarCorDoTime} 
+            key={indice}
+            time={time} 
+            colaboradores={colaboradores.filter(colaborador => colaborador.time 
+            === time.nome)}
+            aoDeletar={deletarColaborador}
+          />
+        )}
+      </section>
+      <Rodape />
     </div>
   );
 }
